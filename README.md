c8b96a8635f62d062879411288f51a36b0e8c3d9
Arthur Danskin <arthurdanskin@gmail.com>
2017-10-11 22:16:33 -0400

    add benchmark

---
+++ README.md
@@ -0,0 +1,51 @@
+Extracted test case for @MalwareMinigun and the Visual C++ Team.
+
+I was complaining in my tweet but let me say THANK YOU for the great (FREE!) IDE and toolchain. I
+use it every day and it is pretty damn good.
+
+In my tweet "I just went from 4fps to 30fps by changing a vector<bool> to a vector<char>. !@!@%$!@#%"
+
+To elaborate, accessing vector<bool> is at least 3 times slower than accessing vector<char> in
+debug builds, even with _ITERATOR_DEBUG_LEVEL=0. It's 4x+ with _ITERATOR_DEBUG_LEVEL=2. I know I
+know it's a debug build, but I need to use the debugger on my game sometimes and it would be nice
+if it was only 2-4x slower (playable) rather than 10+x slower (not playable). As you can see in
+the project settings I already turned off "Basic Runtime Checks" and "Security Check" which helps
+enormously.
+
+This extracted benchmark just repeatedly reads from the vector<bool/char> and writes a value to a
+vector<float> depending on the state.
+
+My game represents various variables about surface of the earth (humidity, temperature,
+population, etc.) using large vector<float>, vector<bool>, etc. where each vector item is mapped
+to a position/pixel on the earth's surface. It then iterates through these vectors every frame
+doing a numerical simulation - pretty similar to the benchmark.
+
+
+Program output on my machine (Debug/x64 build)
+
+initializing
+running bools
+0: 72.27ms
+1: 68.89ms
+2: 77.40ms
+3: 70.94ms
+4: 69.86ms
+5: 69.70ms
+6: 66.22ms
+7: 66.93ms
+8: 66.94ms
+9: 67.18ms
+running chars
+0: 18.69ms
+1: 18.53ms
+2: 18.79ms
+3: 24.73ms
+4: 18.18ms
+5: 18.21ms
+6: 18.83ms
+7: 19.42ms
+8: 18.14ms
+9: 18.91ms
+
+bool avg: 69.63ms
+char avg: 19.24ms
---
+++ VectorBool/VectorBool.cpp
@@ -0,0 +1,88 @@
+// VectorBool.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#define _ITERATOR_DEBUG_LEVEL 0
+#include "windows.h"
+#include <vector>
+
+static double CurrentTime()
+{
+	static LARGE_INTEGER Frequency = { };
+	if (Frequency.QuadPart == 0)
+		QueryPerformanceFrequency(&Frequency);
+	static LARGE_INTEGER StartTime = { };
+	if (StartTime.QuadPart == 0)
+		QueryPerformanceCounter(&StartTime);
+
+	LARGE_INTEGER Time;
+	QueryPerformanceCounter(&Time);
+
+	return double(Time.QuadPart - StartTime.QuadPart) / Frequency.QuadPart;
+}
+
+static void report(const char* fmt, ...)
+{
+	va_list vl;
+	va_start(vl, fmt);
+	char buf[512];
+	vsnprintf(buf, 512, fmt, vl);
+	printf("%s", buf);
+	OutputDebugStringA(buf);
+	va_end(vl);
+}
+
+int main()
+{
+	report("initializing\n");
+	std::vector<float> float_vec;
+	std::vector<char> char_vec;
+	std::vector<bool> bool_vec;
+
+	const int kVecSize = 1000000;
+	const int kIterations = 10;
+
+	float_vec.resize(kVecSize);
+	char_vec.resize(kVecSize);
+	bool_vec.resize(kVecSize);
+
+	for (int i = 0; i < kVecSize; i++)
+	{
+		char_vec[i] = rand() > RAND_MAX / 2;
+		bool_vec[i] = rand() > RAND_MAX / 2;
+	}
+
+	report("running bools\n");
+	double bool_avg_time = 0;
+	for (int i = 0; i < kIterations; i++)
+	{
+		double s = CurrentTime();
+		for (int j=0; j<kVecSize; j++)
+			float_vec[i] = bool_vec[i] ? 1.f : 0.f;
+		double t = (CurrentTime() - s);
+		bool_avg_time += t;
+		report("%d: %.2fms\n", i, t * 1000.0);
+	}
+	bool_avg_time /= kIterations;
+
+	report("running chars\n");
+	double char_avg_time = 0;
+	for (int i = 0; i < kIterations; i++)
+	{
+		double s = CurrentTime();
+		for (int j = 0; j<kVecSize; j++)
+			float_vec[i] = char_vec[i] ? 1.f : 0.f;
+		double t = (CurrentTime() - s);
+		char_avg_time += t;
+		report("%d: %.2fms\n", i, t * 1000.0);
+	}
+	char_avg_time /= kIterations;
+
+	report("\n");
+	report("bool avg: %.2fms\n", bool_avg_time * 1000.0);
+	report("char avg: %.2fms\n", char_avg_time * 1000.0);
+	report("\n");
+
+    return 0;
+}
+
